3abeda717363395aceadeafd77fed5a1c47f3ee3
Macbook <galvaopp@gmail.com>
2020-10-06 23:31:08 -0300

    Adicionado Link - React Router Dom

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
 
 import { Container } from './styles';
 
+interface RepositoryParams {
+  repository: string;
+}
+
 const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+
   return (
     <Container>
-      <h1>Repository</h1>
+      <h1>Repository: {params.repository}</h1>
     </Container>
   );
 };
